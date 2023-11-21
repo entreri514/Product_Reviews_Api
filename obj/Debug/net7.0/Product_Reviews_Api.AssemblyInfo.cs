613a63ac425cf134a1d0c77982c97f263b455a61
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-21 14:57:32 -0600

    Added postman Get all

--- obj/Debug/net7.0/Product_Reviews_Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Product_Reviews_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371f38f0e6d6fd577d1939329f804ef1b117aca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
