acb18fd1bbaf93c218d47baa002c445aabd03a19
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-21 14:35:01 -0600

    Created tables via migration

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Product_Reviews_Api.Models
 {
     public class Review
     {
-        [ForeignKey]
+        [Key]
         public int Id { get; set; }
         public string Text { get; set; }
         public int Rating { get; set; }
+        [ForeignKey("Product")]
         public int ProductId { get; set; }
         public Product Product { get; set; }
     }
--- obj/Debug/net7.0/Product_Reviews_Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Product_Reviews_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f8e83d3ba40ced351c00aae9c2fcad72704b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product_Reviews_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
