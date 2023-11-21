a47b325b3278a5e6246b5d0b51d9f99e9e0a9974
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-21 16:55:42 -0600

    Input code for DTO classes.

---
+++ DTO/ProgramDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Reviews_Api.DTO
+{
+    public class ProgramDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ICollection<ReviewDTO> Reviews { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
